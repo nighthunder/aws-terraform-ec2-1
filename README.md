da4c997ccdd5b28397f06a20aa1294ac19dd0a99
Mayara Morais <mrmo@gft.com>
2023-02-14 16:04:53 -0300

    Adding a readme.md

---
+++ README.md
@@ -0,0 +1,19 @@
+===============================================================
+
+Terraform EC2 creation
+
+===============================================================
+
+<h1> Attaching to the instance: </h1>
+<ul>
+    <li>AMI</li>
+    <li>Public key to the</li>
+    <li>Security groups</li>
+    <li>Subnet and vpc</li>
+    <li>Tags</li>
+</ul>
+
+<p>Docs:</p> <a href="https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance">https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance</a>
+
+
+
--- main.tf
+++ main.tf
@@ -33,12 +33,25 @@ resource "aws_security_group" "k8s-sg"{
 }
 
 resource "aws_instance" "kubernetes-worker"{
+    ami = "ami-007868005aea67c54"
     instance_type = "t3.medium"
     key_name= "k8s-key"
     count = 2
     tags = {
         name = "k8s"
         type = "worker"
+    }
+    security_groups = ["${aws_security_group.k8s-sg.name}"]
+}
+
+resource "aws_instance" "kubernetes-master"{
+    ami = "ami-007868005aea67c54"
+    instance_type = "t3.medium"
+    key_name= "k8s-key"
+    count = 2
+    tags = {
+        name = "k8s"
+        type = "master"
     }
     security_groups = ["${aws_security_group.k8s-sg.name}"]
 }
